eefb840302434c88e5cedef0f32d78934922ea2f
priyanshu-1987 <atulkumarg1987@gmail.com>
2026-06-28 21:43:54 +0530

    m third day learning

---
+++ pri3.js
@@ -0,0 +1,50 @@
+
+let score="33"
+console.log(typeof score); //string
+console.log(typeof (score));//string
+let valueInNumber=Number(score)
+console.log(valueInNumber);//33
+console.log(typeof valueInNumber);//number
+let a="33abc"
+console.log(typeof a);//string
+let b=Number(a)
+console.log(b);//NaN
+console.log(typeof b);//number
+let c=null
+let d=Number(c)
+console.log(d);//0
+console.log(typeof d);//number
+let e=true
+let f=Number(e)
+console.log(f);//1
+console.log(typeof f);//number
+let g=1;
+let h=Boolean(g);
+console.log(h);//true
+
+let m=""
+let n=Boolean(m)
+console.log(n);//false
+console.log(typeof n);//boolean
+let o="hello"
+let p=Boolean(o)
+console.log(p);//true
+let q=33
+let r=String(q)
+console.log(r);//33
+console.log(typeof r);//string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
