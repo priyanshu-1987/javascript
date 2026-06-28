03524a4a6dfcb5e8e447f7fb17c1db0baa35709f
priyanshu-1987 <atulkumarg1987@gmail.com>
2026-06-28 21:40:07 +0530

    basic ja

---
+++ pri1.js
@@ -0,0 +1,11 @@
+const accountId=144553
+let accountEmail="priyanshu@gmail.com"
+var accountPassword="12345"
+accountCity="Gorakhpur"
+let accountState;
+//accountId=2 not valid
+accountEmail="hchc@gmail.com"
+accountPassword="09876"
+accountCity="Delhi"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
