110cc4a1d8c3781133d0bbfb3654e4034e8b4590
priyanshu-1987 <atulkumarg1987@gmail.com>
2026-06-28 21:42:37 +0530

    my second day learning

---
+++ pri2.js
@@ -0,0 +1,27 @@
+"use strict"
+const console = require("node:console")
+
+  //treat all js code as newer version
+// alert(3+3) //we are using nodejs not browser
+console.log(3 +   
+    3
+) //code readability should be high
+console.log("hello")
+
+let name="priyanshu"
+let age=21
+let isLoggedIn=false
+console.log(typeof name)
+console.log(typeof "Priyanshu")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof isLoggedIn)
+//number =>2 to power 53
+//bigint
+//string =>" " or ' '
+//boolean=> true/false
+//null =>standalon value
+//undefined=>mean not value assign
+// symbol=>unique
+//object
